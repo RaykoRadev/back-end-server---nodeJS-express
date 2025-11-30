3fd5283b634168c775b7d571e31de27abae4fa2b
RaykoRadev <christmas898@gmail.com>
2025-11-30 12:40:01 +0100

    updates

--- src/config/dbConfing.js
+++ src/config/dbConfing.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 async function initDatabase() {
-    const dbUrl = `mongodb://localhost:27017`;
+    // const dbUrl = `mongodb://localhost:27017`;
     // const atlasUrl = process.env.atlasUrl;
     //todo may need change
     const dbName = "petShelter";
     try {
-        await mongoose.connect(process.env.atlasUrl, { dbName });
+        await mongoose.connect(process.env.DB_URL, { dbName });
         console.log("DB connected succssesfully");
     } catch (err) {
         console.log("Db connection faild");
